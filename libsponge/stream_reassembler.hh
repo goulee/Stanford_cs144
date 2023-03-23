1ce172453f7ad19ee3fb34ee16ab611611acbd47
goulee <3312669435@qq.com>
2023-03-23 22:06:57 +0800

    lab1 deque

--- libsponge/stream_reassembler.hh
+++ libsponge/stream_reassembler.hh
@@ -3,8 +3,10 @@
 
 #include "byte_stream.hh"
 
+#include <algorithm>
 #include <cstdint>
-#include <map>
+#include <deque>
+#include <iostream>
 #include <string>
 
 //! \brief A class that assembles a series of excerpts from a byte stream (possibly out of order,
@@ -12,13 +14,17 @@
 class StreamReassembler {
   private:
     // Your code here -- add private members as necessary.
-    std::map<size_t, std::string> _unassemble_strs;
-    size_t _next_assembled_idx;
-    size_t _unassembled_bytes_num;
-    size_t _eof_idx;
+    size_t unass_base;        //!< The index of the first unassembled byte
+    size_t unass_size;        //!< The number of bytes in the substrings stored but not yet reassembled
+    bool _eof;                //!< The last byte has arrived
+    std::deque<char> buffer;  //!< The unassembled strings
+    std::deque<bool> bitmap;  //!< buffer bitmap
 
     ByteStream _output;  //!< The reassembled in-order byte stream
     size_t _capacity;    //!< The maximum number of bytes
+
+    void check_contiguous();
+    size_t real_size(const std::string &data, const size_t index);
 
   public:
     //! \brief Construct a `StreamReassembler` that will store up to `capacity` bytes.
@@ -51,6 +57,9 @@ class StreamReassembler {
     //! \brief Is the internal state empty (other than the output stream)?
     //! \returns `true` if no substrings are waiting to be assembled
     bool empty() const;
+
+    //! The acknowledge index of the stream, i.e., the index of the next interested substring
+    size_t ack_index() const;
 };
 
 #endif  // SPONGE_LIBSPONGE_STREAM_REASSEMBLER_HH
